ece0125bbb00f035a97e1fec6dd965825c8d52d6
k-arunreddy <arunreddy248762@gmail.com>
2025-06-30 19:52:56 +0530

    Created Models for SQL Tables

---
+++ Models/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveRequestBackend.Models
+{
+    public class LeaveType
+    {
+        public int LeaveTypeID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
